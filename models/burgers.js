94d57f97270c308d56a00b13d79fed3662d6df7a
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-02-20 18:27:47 -0500

    change false to 0

--- models/burgers.js
+++ models/burgers.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   const Burgers = sequelize.define("Burgers", {
     name: DataTypes.STRING,
-    isEaten: { type: DataTypes.BOOLEAN, default: false }
+    isEaten: { type: DataTypes.BOOLEAN, default: 0}
   });
   return Burgers;
 };
