9fe093d0ed88007b17890cbd17d0f9569b603d59
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-02-20 18:27:40 -0500

    Define routes

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -2,10 +2,40 @@
 // =============================================================
 //                          ROUTES
 // =============================================================
-
-module.exports = function(app) {
-  // GET ROUTE FOR VIEWING INVENTORY
-  app.get("/", function(req, res) {
+// Initialize DB
+const db = require("../models");
+module.exports = app => {
+  // Get route for viewing burgers
+  app.get("/", (req, res) => {
     res.render("index");
+  });
+
+  app.get("/burgers", (req, res) => {
+    db.Burgers.findAll({}).then(burgers => {
+      res.json(burgers);
+    });
+  });
+
+  app.post("/eat/:id", (req, res) => {
+    db.Burgers.update({ isEaten: 1 }, { where: { id: req.params.id } }).then(
+      updated => {
+        res.json(updated);
+      }
+    );
+  });
+
+  app.post("/uneat/:id", (req, res) => {
+    db.Burgers.update({ isEaten: 0 }, { where: { id: req.params.id } }).then(
+      updated => {
+        res.json(updated);
+      }
+    );
+  });
+
+  app.post("/burgers", (req, res) => {
+    console.log(req.body);
+    db.Burgers.create({ name: req.body.burger }).then(() => {
+      res.redirect("/");
+    });
   });
 };
